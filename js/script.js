b8f578b3573295116175d8848b76632430a2b367
GarenChoi <jundan2010@gmail.com>
2022-06-10 12:17:20 +0900

    2022/06/10

--- js/script.js
+++ js/script.js
@@ -12,3 +12,40 @@ document.querySelector(".menu2 .ai").addEventListener("mouseout", () => {
 document.querySelector(".menu1 button img").addEventListener("click", () => {
   document.querySelector(".list").classList.toggle("active");
 });
+
+//banner slide
+function bannerSlide(){
+  let slideIndex = 0;
+        setInterval(() => {
+          
+          if (slideIndex == 4) {
+            document.querySelector(".slide_inner").style.transition = "none";
+            slideIndex = 0;
+            document.querySelector(".slide_inner").style.transform = `translate(${
+            -420 * slideIndex
+            }px)`;
+            setTimeout(()=>{
+              document.querySelector(".slide_inner").style.transition = "transform 0.5s";
+              slideIndex++;
+              document.querySelector(".slide_inner").style.transform = `translate(${
+              -420 * slideIndex
+              }px)`;
+              document.querySelector(".slide_index span").innerText = slideIndex+1;
+            })
+            
+          } else {
+            slideIndex++;
+            document.querySelector(".slide_inner").style.transform = `translate(${
+            -420 * slideIndex
+            }px)`;
+          }
+          
+          if(slideIndex==4){
+            document.querySelector(".slide_index span").innerText = 1;
+          } else {
+            document.querySelector(".slide_index span").innerText = slideIndex +1;
+          }
+        }, 3000);
+
+}
+bannerSlide();
